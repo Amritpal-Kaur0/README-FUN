dbd36df3941f97dcc49c5d1d6c8a6308c6689514
Amrit1-kaur <amrit.gill3005@gmail.com>
2023-05-04 15:22:42 +0530

    Readme genereator

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# README-FUN
+# Readme Generator
+# ![License](https://img.shields.io/badge/license-MIT-Orange)
+
+## Table of Content
+
+ - [Project Description](#Description)
+ - [Usage](#Usage)
+ - [Contribution](#Contribution)
+ - [Installation](#Instation)
+ - [Questions](#Questions)
+ - [Licenece](#Licence)
+
+
+
+## Description
+  This application will assists users to generate professional and high-quality README.
+
+## Usage
+ Node.js installed and run node index.js to acess this application.
+
+## Installation
+   run         npm init          npm install --save inquirer@8.2.4
+
+## Author 
+ Amrit
+
+## Contribution
+ Contribution ,issues and feature requests are Welcome.
+ Feel free to Check Feel free to Create GitHub Issue Page or Email me for Collaboration.
+
+
+  ## License
+
+This project is licensed under the MIT license. For more information about the license, go to [License](https://choosealicense.com/licenses/mit/).
+
+
+## Test
+ 
+
+## Questions
+ Email -Amrit.gill3005@gmail.com
+ Github - [Amritpal-Kaur0]      Link -(https://github.com/Amritpal-Kaur0)
+ LinkedIn -Amritpal
+  
---
+++ index.js
@@ -0,0 +1,87 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./lib/Generate.js');
+
+console.log(" 😍 Welcome to README Generator App")
+
+const questions =[
+    {
+      type: 'input',
+      name: 'Title',
+      message: '✅ What is the Title of your Project?',
+    },
+    {
+      type: 'input',
+      name: 'Description',
+      message: '📨 Provide a short description explaining  your Project?',
+    },
+    {
+      type: 'input',
+      name: 'author',
+      message: '👤 Who is the  author of this Project?',
+    },
+    {
+      type: 'input',
+      name: 'Usage',
+      message: '🔧 What is the Usage of this Project ?',
+    },
+    {
+      type: 'input',
+      name: 'Installation',
+      message: '🪜 What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: '👪 How can users contribute to this project?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: '👪 Enter tests Instructions?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: '👤  Enter your GitHub Username(Required)',
+  
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: '📧 For any Queries related this Project Please Email at -',
+    },
+    {
+      type: 'input',
+      name: 'LinkedIn',
+      message: '📧 Enter Your LinkedIn Username? -',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: '🪪 Choose a license that will best suits your projects?',
+      choices:['C++','Apache','Unlicense','Node','MIT',]
+    },
+  ]
+
+// TODO: Create a Function to write README file. 
+function writeToFile(fileName, answers) {
+      fs.writeFile(fileName, answers, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`${fileName} has been generated successfully!`);
+        }
+      });
+ }
+  
+// TODO: Create a function to initialize the App 
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+      const file =generateMarkdown(answers);
+      writeToFile("README.md",file)
+    })
+}
+
+init();
